827c777bab949941af200c2fa88a5547a67b267c
davidcoats <david@getsafetycone.com>
2021-12-30 11:50:12 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0121.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0121.Construction
+{
+    /// <summary>
+    /// Construction project for IRegexPattern and other regular expression-related extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0121/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0121
+{
+    /// <summary>
+    /// IRegexPattern and other regular expression-related extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
